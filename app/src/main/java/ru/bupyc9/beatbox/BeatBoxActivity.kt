9f6b02f10054d7358988cba250ea48c05d9a9d18
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-08-20 21:29:37 +0300

    add kotlin

---
+++ app/src/main/java/ru/bupyc9/beatbox/BeatBoxActivity.kt
@@ -0,0 +1,12 @@
+package ru.bupyc9.beatbox
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BeatBoxActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_beat_box)
+    }
+}
